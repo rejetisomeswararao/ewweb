ea9efaf7d6e668f20a6ca3248d4b4383fc465652
rejetisomeswararao <rejetisomeswararao@gmail.com>
2025-09-28 17:01:51 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function GOLINK(){
-    open("https://www.youtube.com/watch?v=FF7aqV2ybog&list=RDFF7aqV2ybog&start_radio=1")
-}
-function secondsong(){
-    open("https://www.youtube.com/watch?v=Nfp0hXlvguw&list=RDGMEMWO-g6DgCWEqKlDtKbJA1GwVMNfp0hXlvguw&start_radio=1")
-}
